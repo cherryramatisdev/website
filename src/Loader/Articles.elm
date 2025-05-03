2795402b14fd0f0748b58259c2f8ac067b6ff641
Cherry Ramatis <cherry.ramatis@icloud.com>
2025-05-03 05:30:44 -0300

    Begin work adding a loader for the articles

---
+++ src/Loader/Articles.elm
@@ -0,0 +1,45 @@
+module Loader.Articles exposing (..)
+
+type alias Article = {
+  "type_of": "article",
+  "id": 1899196,
+  "title": "I'm looking for a FullStack Software Developer Role",
+  "description": "Hey y'all, hope you've been doing and feeling great! My name is Cherry Ramatis, and I'm currently...",
+  "readable_publish_date": "Jun 25 '24",
+  "slug": "im-looking-for-a-fullstack-software-developer-role-417m",
+  "path": "/cherryramatis/im-looking-for-a-fullstack-software-developer-role-417m",
+  "url": "https://dev.to/cherryramatis/im-looking-for-a-fullstack-software-developer-role-417m",
+  "comments_count": 37,
+  "public_reactions_count": 330,
+  "collection_id": null,
+  "published_timestamp": "2024-06-25T17:09:10Z",
+  "language": "en",
+  "subforem_id": null,
+  "positive_reactions_count": 330,
+  "cover_image": "https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fgytnfg0d2iqev3g7t53b.png",
+  "social_image": "https://media2.dev.to/dynamic/image/width=1000,height=500,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fgytnfg0d2iqev3g7t53b.png",
+  "canonical_url": "https://dev.to/cherryramatis/im-looking-for-a-fullstack-software-developer-role-417m",
+  "created_at": "2024-06-24T17:50:06Z",
+  "edited_at": null,
+  "crossposted_at": null,
+  "published_at": "2024-06-25T17:09:10Z",
+  "last_comment_at": "2025-01-04T07:06:48Z",
+  "reading_time_minutes": 2,
+  "tag_list": [
+    "programming",
+    "career",
+    "webdev",
+    "coding"
+  ],
+  "tags": "programming, career, webdev, coding",
+  "user": {
+    "name": "Cherry Ramatis",
+    "username": "cherryramatis",
+    "twitter_username": "cherryramatis",
+    "github_username": "cherryramatisdev",
+    "user_id": 1025053,
+    "website_url": null,
+    "profile_image": "https://media2.dev.to/dynamic/image/width=640,height=640,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1025053%2F6cfe3178-ceaa-4b57-bef0-925fd78baadd.jpeg",
+    "profile_image_90": "https://media2.dev.to/dynamic/image/width=90,height=90,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Fuser%2Fprofile_image%2F1025053%2F6cfe3178-ceaa-4b57-bef0-925fd78baadd.jpeg"
+  }
+}
